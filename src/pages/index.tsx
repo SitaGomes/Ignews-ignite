877933c9b90381c0e0bb86f993107e9bcaaab187
SitaGomes <ansitagomes@gmail.com>
2021-10-09 18:12:23 +0100

    Add: Home styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Header from "next/head"
 
+import style from "./home.module.scss"
 
 export default function Home() {
   return (
@@ -8,6 +9,27 @@ export default function Home() {
       <Header> 
         <title>Home | Ig.News</title>
       </Header>
+
+      <main className={style.Container}>
+        <div className={style.Content}>
+          <img src="/svg/avatar.svg" alt="Girl Coding" />
+          <section>
+            <span>👏 Hey, Welcome</span>
+            <h1>
+              News about the <span>React</span> world
+            </h1>
+            <p>
+              Get acess to all the publications
+              <br />
+              <span>for $9.90 month</span>
+            </p>
+
+            <button>
+              Subscribe now
+            </button>
+          </section>
+        </div>
+      </main>
 
     </>
   )
