6316f901c11d7f23c838e6af39b84148f444680f
SitaGomes <ansitagomes@gmail.com>
2021-11-15 16:41:50 +0100

    Add: stripe customer creation optimization

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,20 +1,65 @@
 import { NextApiRequest, NextApiResponse } from "next";
+
 import { getSession } from "next-auth/client";
+
+import {query as q} from "faunadb"
+
+
+import { fauna } from "../../services/faunadb";
 import { stripe } from "../../services/stripe";
+
+
+interface User {
+    ref: {
+        id: string;
+    },
+    data: {
+        stripe_customer_id: string
+    }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     if (req.method === "POST") {
-
+        //* Getting logged user's data
         const session = await getSession({req}) //Gets user data
 
-        const stripeCustomer = await stripe.customers.create({
-            email: session.user.email,
-            //metadata            
-        })
+        const user = await fauna.query<User>(
+            q.Get(
+                q.Match(
+                    q.Index("user_by_email"),
+                    q.Casefold(session.user.email)
+                )
+            )
+        )
+
+        let customerId = user.data.stripe_customer_id 
+
+        if (!customerId) {
+
+            const stripeCustomer = await stripe.customers.create({
+                email: session.user.email,
+                //metadata            
+            })
+    
+            //* Posting the stripe's customer id in the db
+            await fauna.query(
+                q.Update(
+                    q.Ref(q.Collection("users"), user.ref.id),
+                    {
+                        data: {
+                            stripe_customer_id: stripeCustomer.id
+                        }
+                    }
+                )
+            )
+
+            customerId = stripeCustomer.id //? must always have a value
+        }
+
 
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustomer.id,
+            customer: customerId,
             payment_method_types: ["card"],
             billing_address_collection: "required",
             line_items: [{price: "price_1JitgWDPqecVYF0f9OQovo1w", quantity: 1}],
