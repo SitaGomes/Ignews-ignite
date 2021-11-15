088326c7999618631cd5aea56f0dbe11a8e71cb8
SitaGomes <ansitagomes@gmail.com>
2021-11-15 15:43:49 +0100

    Add: creating stripe customer and session

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,9 +1,41 @@
+import { signIn, useSession } from 'next-auth/client'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
 import style from './style.module.scss'
 
-export const SubscribeButton: React.FC = () => {
+interface SubscribeButtonProps {
+    priceId: string;
+}
+
+export const SubscribeButton: React.FC<SubscribeButtonProps> = ({priceId}) => {
+    const [session] = useSession()
+
+    async function handleSubscribeButton() {
+        if (!session) {
+            signIn("github")
+            return
+        }
+
+        try { 
+            const response = await api.post("/subscribe")
+
+            const {sessionID} = response.data
+        
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({sessionId: sessionID})
+
+        } catch (err) {
+            alert(`${err}`)
+            return
+        }
+    }
 
     return(
-        <button className={style.button}>
+        <button 
+            className={style.button}
+            onClick={handleSubscribeButton}    
+        >
             Subscribe now
         </button>
     )
